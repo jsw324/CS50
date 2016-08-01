a30370977a9d60a36b2a9f8694cad580573ffec0
Jason Walkow <jwalkow@tallience.com>
2016-08-01 02:26:03 +0000

    getting closer on recover.c

--- pset4/jpg/newfiles.c
+++ pset4/jpg/newfiles.c
@@ -5,28 +5,11 @@
 
 int main(void)
 {
-   // printf("digame: ");
+    char buffer[50];
     
-    //string input = GetString();
+    int course = 50;
     
-    FILE* inptr = fopen("card.raw", "r");
+    sprintf(buffer,"CS%d rocks!", course);
     
-    FILE* outptr = fopen("newfile.txt", "w");
-      
-    char content[sizeof(inptr)];
-    
-   // fread(content, sizeof(test), 1, test);
-    
-    while(fread(content, sizeof(inptr), 1, inptr) == 1)
-    {
-        for(int i = 0; i < sizeof(inptr); i++)
-        {
-            printf("%s\n", (content + i));
-            if (strcmp((content + i), "0xff") == 0 && strcmp((content + i + 1), "0xd8") == 0 )
-            {
-                printf("%c\n", *(content + i));
-                fwrite(content, sizeof(inptr), 1, outptr);
-            }
-        }
-    }
+    printf("%s\n", buffer);
 }
--- pset4/jpg/recover.c
+++ pset4/jpg/recover.c
@@ -11,6 +11,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <cs50.h>
+
+//prototype getJpegName();
 
 int main(void)
 {
@@ -18,13 +21,31 @@ int main(void)
   
   unsigned char buffer[512];
   
-  char jpgname = NULL:
+  char jpgName[50];
+  int jpgCounter = 0;
+  
   
   while(fread(buffer, 512, 1, inptr) == 1)
   {
+    FILE* outptr = fopen(jpgName, "w");
     if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] == 0xe0) || (buffer[3] == 0xe1)))
     {
-      printf("found one\n");
+      if(jpgCounter < 10)
+      {
+        sprintf(jpgName, "00%d.jpg", jpgCounter);
+      } else {
+        sprintf(jpgName, "0%d.jpg", jpgCounter);
+      }
+      if((buffer[0] != 0xff) && (buffer[1] != 0xd8) && (buffer[2] != 0xff) && ((buffer[3] != 0xe0) || (buffer[3] != 0xe1)) && (jpgCounter != 0)) 
+      {
+        outptr = fopen(jpgName, "a");
+        fwrite(buffer, 512, 1, outptr);
+      }
+      else 
+      {
+        fclose(outptr);
+        jpgCounter++;
+      }
     }
   }
 }
