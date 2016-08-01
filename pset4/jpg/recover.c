e8401d0aa6c366171b09cf882559a5936f46175c
Jason Walkow <jwalkow@tallience.com>
2016-08-01 16:47:43 +0000

    finished recover.c and it passes all checks!

--- pset4/jpg/recover.c
+++ pset4/jpg/recover.c
@@ -22,30 +22,36 @@ int main(void)
   unsigned char buffer[512];
   
   char jpgName[50];
-  int jpgCounter = 0;
+  int jpgCounter = -1;
   
+  FILE* outptr = NULL;
   
   while(fread(buffer, 512, 1, inptr) == 1)
   {
-    FILE* outptr = fopen(jpgName, "w");
-    if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] == 0xe0) || (buffer[3] == 0xe1)))
-    {
-      if(jpgCounter < 10)
-      {
-        sprintf(jpgName, "00%d.jpg", jpgCounter);
-      } else {
-        sprintf(jpgName, "0%d.jpg", jpgCounter);
-      }
-      if((buffer[0] != 0xff) && (buffer[1] != 0xd8) && (buffer[2] != 0xff) && ((buffer[3] != 0xe0) || (buffer[3] != 0xe1)) && (jpgCounter != 0)) 
-      {
-        outptr = fopen(jpgName, "a");
+     if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] == 0xe0) || (buffer[3] == 0xe1)))
+     {
+       jpgCounter++;
+       if(jpgCounter == 0){
+         sprintf(jpgName, "00%d.jpg", jpgCounter);
+         outptr = fopen(jpgName, "a");
+         fwrite(buffer, 512, 1, outptr);
+       } else {
+         fclose(outptr);
+         if(jpgCounter < 10){
+           sprintf(jpgName, "00%d.jpg", jpgCounter);
+         } else {
+           sprintf(jpgName, "0%d.jpg", jpgCounter);
+         }
+         outptr = fopen(jpgName, "a");
+         fwrite(buffer, 512, 1, outptr);
+       }
+     } else {
+       if(outptr != NULL){
+        //outptr = fopen(jpgName, "a");
         fwrite(buffer, 512, 1, outptr);
-      }
-      else 
-      {
-        fclose(outptr);
-        jpgCounter++;
-      }
-    }
+       }
+     }
   }
+   
+   
 }
