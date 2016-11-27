591e1ae25c53b595281eb45a0877b6f4c66f2517
Jason Walkow <jwalkow@tallience.com>
2016-11-27 21:23:18 +0000

    added portfolio functionality

--- pset7/public/index.php
+++ pset7/public/index.php
@@ -2,8 +2,27 @@
 
     // configuration
     require("../includes/config.php"); 
-
+    
+    $rows = CS50::query("SELECT * FROM portfolio WHERE user_id = ?", $_SESSION["id"]);
+    
+    $positions = [];
+    
+    foreach ($rows as $row)
+    {
+        $stock = lookup($row["symbol"]);
+        if ($stock !== false)
+        {
+            $positions[] = [
+                "name" => $stock["name"],
+                "price" => $stock["price"],
+                "shares" => $row["shares"],
+                "symbol" => $row["symbol"]
+            ];
+        } else {
+            apologize("sorry, can't retreive stock symbol from portfolio");
+        }
+    }
     // render portfolio
-    render("portfolio.php", ["title" => "Portfolio"]);
+    render("portfolio.php", ["title" => "Portfolio", "positions" => $positions]);
 
 ?>
--- pset7/views/portfolio.php
+++ pset7/views/portfolio.php
@@ -1,3 +1,28 @@
-<div>
-    <iframe allowfullscreen frameborder="0" height="315" src="https://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1&iv_load_policy=3&rel=0" width="420"></iframe>
+<div class="container">
+    <table class="table table-striped">
+          <thead class="thead-inverse">
+            <tr>
+              <th>Symbol</th>
+              <th>Name</th>
+              <th>Shares</th>
+              <th>Price</th>
+              <th>TOTAL</th>
+            </tr>
+          </thead>
+    
+          <tbody>
+            <?php foreach ($positions as $position): ?>
+            <tr>
+              <td><?= $position["symbol"] ?></td>
+              <td><?= $position["name"] ?></td>
+              <td><?= $position["shares"] ?></td>
+              <td><?= $position["price"] ?></td>
+              <td><?= $position["price"] * $position["shares"] ?></td>
+            </tr>
+            <?php endforeach ?>
+           
+         </tbody>
+    </table>
 </div>
+
+ 
